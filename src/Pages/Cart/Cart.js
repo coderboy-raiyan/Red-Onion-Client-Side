b7b732450f9ee1290a880f8fb6735b1fc5741195
Raiyan <raiyan5764@gmail.com>
2021-12-29 16:53:54 +0600

    cart being ready

--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -5,7 +5,35 @@ const Cart = () => {
   return (
     <>
       <Header />
-      <h1>This is cart</h1>
+
+      <div className="lg:max-w-6xl lg:mx-auto max-w-3xl mx-4 grid lg:grid-cols-3 md:grid-cols-3 grid-cols-1 py-12">
+        {/* Cart items section */}
+        <div>
+          <h1>This is cart</h1>
+        </div>
+
+        {/* Information section */}
+        <div className="col-span-2">
+          <div>
+            <form className="w-2/3 ml-auto flex flex-col justify-center items-center space-y-6">
+              <div className="flex flex-col space-y-8 w-full">
+                <input className="form-input" type="text" placeholder="Name" />
+                <input
+                  className="form-input"
+                  type="text"
+                  placeholder="Address"
+                />
+                <input
+                  className="form-input"
+                  type="text"
+                  placeholder="House no"
+                />
+              </div>
+              <button className="primary-btn w-full rounded py-4">Save</button>
+            </form>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
