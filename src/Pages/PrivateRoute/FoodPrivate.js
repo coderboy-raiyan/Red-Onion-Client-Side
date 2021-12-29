bbc800de2dce5f0af847a7ec622d9ab5f459322f
Raiyan <raiyan5764@gmail.com>
2021-12-29 12:09:52 +0600

    fancy loader added

--- src/Pages/PrivateRoute/FoodPrivate.js
+++ src/Pages/PrivateRoute/FoodPrivate.js
@@ -1,17 +1,29 @@
+import { css } from "@emotion/react";
 import React from "react";
 import { useSelector } from "react-redux";
 import { Redirect, Route } from "react-router-dom";
+import GridLoader from "react-spinners/GridLoader";
 import {
   selectLoading,
   selectUser,
 } from "./../../Reducers/userSlice/userSlice";
+
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`;
 
 const FoodPrivate = ({ children, ...rest }) => {
   const user = useSelector(selectUser);
   const loading = useSelector(selectLoading);
 
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <GridLoader color={"#F15858"} css={override} size={50} />;
+      </div>
+    );
   }
 
   return (
